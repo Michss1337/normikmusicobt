60c453b1526b8a1f76209f6340e7fcc01f88fbcc
Michss1337 <52043237+Michss1337@users.noreply.github.com>
2019-06-20 17:25:53 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ exports.TOKEN = 'NTM1NDMxMzk1Njg3MDA2MjA5.XQuiPA.0AmdKIk0GtQlwpxC_UOVvW7PzpU';
 
 exports.PREFIX = '$';
 
-exports.GOOGLE_API_KEY = 'AIzaSyCODvDzlT_GJCQuOnkKo8Zr-Fv2pw1BCXI';
+exports.GOOGLE_API_KEY = 'AIzaSyBoHxUj-GeFE1FbhvNeGd3NQBwOaXguXRs';
